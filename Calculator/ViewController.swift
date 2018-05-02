0468e4f40dd60a4bdc2e4d11c867780ec287c9d9
Nguyen Nguu <nguund@Mac-cua-Nguyen.local>
2018-05-02 15:55:47 +0700

    update

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,65 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var numberOnScreen:Double = 0;
-    var previousNumber:Double = 0;
-    var performingMatch = false;
-    var operation = 0;
+    var stringWithMathematicalOperation: String = ""
+    var result: Double = 0
+    var pheptoan = 1
+    var phepdem = 0
     
     @IBOutlet weak var label: UILabel!
-    
-    @IBAction func numbers(_ sender: UIButton) {
-        if performingMatch == true {
-            label.text = String(sender.tag - 1)
-            numberOnScreen = Double(label.text!)!
-            performingMatch = false
+
+    @IBAction func button(_ sender: UIButton) {
+        //=
+        if sender.tag == 16 && phepdem == 0 && pheptoan == 1 && label.text != ""{
+            let exp: NSExpression = NSExpression(format: stringWithMathematicalOperation)
+            result = exp.expressionValue(with: nil, context: nil) as! Double
+            label.text = String(result)
         }
-        else {
-            label.text = label.text! + String(sender.tag - 1)
-            numberOnScreen = Double(label.text!)!
-        }
-    }
-    
-    @IBAction func buttons(_ sender: UIButton) {
-        if(label.text != "" && sender.tag != 11 && sender.tag != 16){
-            previousNumber = Double(label.text!)!
-            if sender.tag == 12 { //Chia
-                label.text = "/";
-            }
-            else if sender.tag == 13 { //Nhan
-                label.text = "x";
-            }
-            else if sender.tag == 14 { //Tru
-                label.text = "-";
-            }
-            else if sender.tag == 15 { //Cong
-                label.text = "+";
-            }
-            
-            operation = sender.tag
-            performingMatch = true;
-        }
-        else if sender.tag == 16 { //=
-            if operation == 12 {
-                label.text = String(previousNumber / numberOnScreen)
-            }
-            else if operation == 13 {
-                label.text = String(previousNumber * numberOnScreen)
-            }
-            else if operation == 14 {
-                label.text = String(previousNumber - numberOnScreen)
-            }
-            else if operation == 15 {
-                label.text = String(previousNumber + numberOnScreen)
-            }
-        }
+        //C
         else if sender.tag == 11 {
-            label.text = ""
-            previousNumber = 0
-            numberOnScreen = 0
-            operation = 0
+            result = 0
+            stringWithMathematicalOperation = ""
+            label.text = String(result)
         }
     }
+    
+    @IBAction func number(_ sender: UIButton) {
+        //0 -> 9
+        if sender.tag < 12 {
+            stringWithMathematicalOperation += String(sender.tag - 1)
+            label.text = stringWithMathematicalOperation
+            pheptoan = 1
+        }
+        //+
+        else if label.text != "" && sender.tag == 15 && pheptoan == 1 {
+            stringWithMathematicalOperation += "+"
+            label.text = stringWithMathematicalOperation
+            pheptoan = 0
+        }
+        //-
+        else if label.text != "" && sender.tag == 14 && pheptoan == 1 {
+            stringWithMathematicalOperation += "-"
+            label.text = stringWithMathematicalOperation
+            pheptoan = 0
+        }
+        //*
+        else if label.text != "" && sender.tag == 13 && pheptoan == 1 {
+            stringWithMathematicalOperation += "*"
+            label.text = stringWithMathematicalOperation
+            pheptoan = 0
+        }
+        ///
+        else if label.text != "" && sender.tag == 12 && pheptoan == 1 {
+            stringWithMathematicalOperation += "/"
+            label.text = stringWithMathematicalOperation
+            pheptoan = 0
+        }
+        //.
+        else if label.text != "" && sender.tag == 17 && pheptoan == 1 {
+            stringWithMathematicalOperation += "."
+            label.text = stringWithMathematicalOperation
+            pheptoan = 0
+        }
+        //(
+        else if label.text != "" && sender.tag == 18{
+            stringWithMathematicalOperation += "("
+            label.text = stringWithMathematicalOperation
+            pheptoan = 0
+            phepdem += 1
+        }
+        //)
+        else if label.text != "" && sender.tag == 19{
+            stringWithMathematicalOperation += ")"
+            label.text = stringWithMathematicalOperation
+            pheptoan = 1
+            phepdem -= 1
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
