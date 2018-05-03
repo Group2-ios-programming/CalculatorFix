1cf1ed0d05acfe5d850bccb7bdd8b19a17734045
Nguyen Nguu <nguund@Mac-cua-Nguyen.local>
2018-05-03 22:44:09 +0700

    update tiep

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,27 @@ class ViewController: UIViewController {
 
     var stringWithMathematicalOperation: String = ""
     var result: Double = 0
-    var pheptoan = 1
-    var phepdem = 0
+    var count = 0
+    var temp = 0 //0 empty; 1 number; 2 operation; 3 .; 4 (; 5 )
     
     @IBOutlet weak var label: UILabel!
 
     @IBAction func button(_ sender: UIButton) {
         //=
-        if sender.tag == 16 && phepdem == 0 && pheptoan == 1 && label.text != ""{
+        /*
+         Dieu kien:
+            + So luong ngoac mo == ngoac dong (count = 0)
+            + Phan tu cuoi cung phai la so hoac ngoac dong (temp == 1 || temp == 5)
+            + Chuoi phai khac rong (label.text != "")
+        */
+        if sender.tag == 16 && count == 0 && (temp == 1 || temp == 5) && label.text != ""{
             let exp: NSExpression = NSExpression(format: stringWithMathematicalOperation)
             result = exp.expressionValue(with: nil, context: nil) as! Double
             label.text = String(result)
         }
         //C
         else if sender.tag == 11 {
+            temp = 0
             result = 0
             stringWithMathematicalOperation = ""
             label.text = String(result)
@@ -34,54 +41,89 @@ class ViewController: UIViewController {
     
     @IBAction func number(_ sender: UIButton) {
         //0 -> 9
-        if sender.tag < 12 {
+        /*
+         Dieu kien:
+            + Phan tu cuoi cung phai khac ngoac dong (temp != 5)
+         */
+        if sender.tag < 12 && temp != 5{
             stringWithMathematicalOperation += String(sender.tag - 1)
             label.text = stringWithMathematicalOperation
-            pheptoan = 1
+            temp = 1
         }
         //+
-        else if label.text != "" && sender.tag == 15 && pheptoan == 1 {
+        /*
+        Dieu kien:
+             + Chuoi phai khac rong (label.text != "")
+             + Phan tu cuoi cung phai khac operation (temp != 2)
+             + Phan tu cuoi cung phai khac dau . (temp != 3)
+        */
+        else if label.text != "" && sender.tag == 15 && temp != 2 && temp != 3 {
             stringWithMathematicalOperation += "+"
             label.text = stringWithMathematicalOperation
-            pheptoan = 0
+            temp = 2
         }
         //-
-        else if label.text != "" && sender.tag == 14 && pheptoan == 1 {
+        /*
+             Dieu kien tuong tu dau +
+        */
+        else if label.text != "" && sender.tag == 14 && temp != 2 && temp != 3 {
             stringWithMathematicalOperation += "-"
             label.text = stringWithMathematicalOperation
-            pheptoan = 0
+            temp = 2
         }
         //*
-        else if label.text != "" && sender.tag == 13 && pheptoan == 1 {
+        /*
+             Dieu kien tuong tu dau +
+        */
+        else if label.text != "" && sender.tag == 13 && temp != 2 && temp != 3 {
             stringWithMathematicalOperation += "*"
             label.text = stringWithMathematicalOperation
-            pheptoan = 0
+            temp = 2
         }
         ///
-        else if label.text != "" && sender.tag == 12 && pheptoan == 1 {
+        /*
+             Dieu kien tuong tu dau +
+        */
+        else if label.text != "" && sender.tag == 12 && temp != 2 && temp != 3 {
             stringWithMathematicalOperation += "/"
             label.text = stringWithMathematicalOperation
-            pheptoan = 0
+            temp = 2
         }
         //.
-        else if label.text != "" && sender.tag == 17 && pheptoan == 1 {
+        /*
+        Dieu kien:
+             + Chuoi phai khac rong (label.text != "")
+             + Phan tu cuoi cung phai la so (temp == 1)
+        */
+        else if label.text != "" && sender.tag == 17 && temp == 1 {
             stringWithMathematicalOperation += "."
             label.text = stringWithMathematicalOperation
-            pheptoan = 0
+            temp = 3
         }
         //(
-        else if label.text != "" && sender.tag == 18{
+        /*
+        Dieu kien:
+             + Phan tu cuoi cung phai la empty hoac operation (temp == 0 || temp == 2)
+        */
+        else if sender.tag == 18 && (temp == 0 || temp == 2){
             stringWithMathematicalOperation += "("
             label.text = stringWithMathematicalOperation
-            pheptoan = 0
-            phepdem += 1
+            temp = 4
+            count += 1
         }
         //)
-        else if label.text != "" && sender.tag == 19{
+        /*
+        Dieu kien:
+             + Chuoi phai khac rong (label.text != "")
+             + Phan tu cuoi cung phai khac operation (temp != 2)
+             + Phan tu cuoi cung phai khac dau . (temp != 3)
+             + So luong ngoac mo > so luong ngoac dong (count > 0)
+        */
+        else if label.text != "" && sender.tag == 19 && temp != 2 && temp != 3 && count > 0{
             stringWithMathematicalOperation += ")"
             label.text = stringWithMathematicalOperation
-            pheptoan = 1
-            phepdem -= 1
+            temp = 5
+            count -= 1
         }
     }
     
